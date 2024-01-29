476487b4795ec16cc7394674776ede306732ce82
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-01-29 14:39:13 -0800

    Username html validation

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//get main elements:
+//registration form
+const regForm = document.getElementById("registration");
+//login form 
+const loginForm = document.getElementById("login");
+//box for validation error
+const errorDisplay = document.getElementById("errorDisplay");
